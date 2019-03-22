c3cb79a004ebed3a133f9d29a65e18e582ec704e
JessieD66 <35700067+JessieD66@users.noreply.github.com>
2019-03-22 13:16:29 +0100

    Add files via upload

--- Robot2.java
+++ Robot2.java
@@ -5,8 +5,8 @@ import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.Color;
+import lejos.utility.Delay;
 
 public class Robot2 {
 	EV3MediumRegulatedMotor motorArm;
@@ -14,7 +14,6 @@ public class Robot2 {
 	EV3LargeRegulatedMotor motorRight;
 	EV3ColorSensor colorSensorLeft;
 	EV3ColorSensor colorSensorRight;
-	EV3ColorSensor colorSensorFront;
 	EV3ColorSensor colorSensorSide;
 	String[] orientations;
 
@@ -24,7 +23,6 @@ public class Robot2 {
 		this.motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
 		this.colorSensorLeft = new EV3ColorSensor(SensorPort.S1);
 		this.colorSensorRight = new EV3ColorSensor(SensorPort.S2);
-		this.colorSensorFront = new EV3ColorSensor(SensorPort.S3);
 		this.colorSensorSide = new EV3ColorSensor(SensorPort.S4);
 		this.orientations = new String[] { "", "", "", "" }; // index: 0: east, 1: south, 2: west, 3: north
 	}
@@ -34,13 +32,28 @@ public class Robot2 {
 		robot.colorSensorSide.setFloodlight(Color.WHITE);
 		LCD.drawString("running, press enter...", 0, 0);
 		Button.ENTER.waitForPress();
-		while (robot.orientations[3] == "") {
-			robot.followLine();
-			robot.findOrientations();
-		}
+		/* 1. fahre bis zum ersten Block
+		 * 2. lies Farbe des ersten Blocks
+		 * 3. fahre zum zweiten Block
+		 * 4. lies Farbe des zweiten Blocks ...
+		 */
+		LCD.clear();
+		robot.drive(5000);
+		robot.orientations[0] = robot.getColorName(robot.colorSensorSide.getColorID());
+		//robot.findOrientations();
 		LCD.drawString("East " + robot.orientations[0], 0, 0);
+		robot.drive(3000);
+		robot.orientations[1] = robot.getColorName(robot.colorSensorSide.getColorID());
+		//robot.findOrientations();
 		LCD.drawString("South " + robot.orientations[1], 0, 1);
+		robot.drive(3000);
+		robot.orientations[2] = robot.getColorName(robot.colorSensorSide.getColorID());
+		//robot.findOrientations();
 		LCD.drawString("West " + robot.orientations[2], 0, 2);
+		robot.drive(3000);
+	
+		robot.orientations[3] = robot.getColorName(robot.colorSensorSide.getColorID());
+		//robot.findOrientations();
 		LCD.drawString("North " + robot.orientations[3], 0, 3);
 		LCD.drawString("Turn Red:  " + robot.toTurn("RED"), 0, 4);
 		LCD.drawString("Turn Green:  " + robot.toTurn("GREEN"), 0, 5);
@@ -48,6 +61,15 @@ public class Robot2 {
 		LCD.drawString("Turn Yellow:  " + robot.toTurn("YELLOW"), 0, 7);
 		Button.ENTER.waitForPress();
 	}
+	
+	public void drive(int time) {
+		motorLeft.setSpeed(100);
+		motorRight.setSpeed(100);
+		motorRight.forward();
+		motorLeft.forward();
+		Delay.msDelay(time);
+		stop();
+		}
 
 	public void followLine() {
 		motorLeft.setSpeed(100);
@@ -63,6 +85,11 @@ public class Robot2 {
 			motorLeft.forward();
 		}
 
+	}
+	
+	public void stop() {
+		motorRight.stop(true);
+		motorLeft.stop(true);
 	}
 
 	public int getColorId(String color) {
@@ -148,7 +175,7 @@ public class Robot2 {
 		}
 		// first index: orientation East, South, West, North
 		// second index: color red, green, blue, yellow
-		boolean[][] turnmatrix = { { true, false, true, false }, { true, true, false, false }, { false, true, true, false }, { true, true, false, false } };
+		boolean[][] turnmatrix = { { true, false, true, false }, { true, true, false, false }, { false, true, false, true }, { false, false, true, true } };
 		return turnmatrix[orientationIndex][colorIndex];
 	}
 }
