a8a77a68ee07ddea41ca65441c736f9363568ffe
JessieD66 <35700067+JessieD66@users.noreply.github.com>
2019-04-05 15:01:05 +0200

    Add files via upload

--- Robot2.java
+++ Robot2.java
@@ -1,4 +1,5 @@
 import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
@@ -9,20 +10,22 @@ import lejos.robotics.Color;
 import lejos.utility.Delay;
 
 public class Robot2 {
-	EV3MediumRegulatedMotor motorArm;
+	EV3MediumRegulatedMotor motorUp;
+	EV3MediumRegulatedMotor motorTurn;
 	EV3LargeRegulatedMotor motorLeft;
 	EV3LargeRegulatedMotor motorRight;
-	EV3ColorSensor colorSensorLeft;
-	EV3ColorSensor colorSensorRight;
+	//EV3ColorSensor colorSensorLeft;
+	//EV3ColorSensor colorSensorRight;
 	EV3ColorSensor colorSensorSide;
 	String[] orientations;
 
 	Robot2() {
-		this.motorArm = new EV3MediumRegulatedMotor(MotorPort.A);
+		this.motorUp = new EV3MediumRegulatedMotor(MotorPort.A);
+		this.motorTurn = new EV3MediumRegulatedMotor(MotorPort.D);
 		this.motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
 		this.motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
-		this.colorSensorLeft = new EV3ColorSensor(SensorPort.S1);
-		this.colorSensorRight = new EV3ColorSensor(SensorPort.S2);
+		//this.colorSensorLeft = new EV3ColorSensor(SensorPort.S1);
+		//this.colorSensorRight = new EV3ColorSensor(SensorPort.S2);
 		this.colorSensorSide = new EV3ColorSensor(SensorPort.S4);
 		this.orientations = new String[] { "", "", "", "" }; // index: 0: east, 1: south, 2: west, 3: north
 	}
@@ -32,40 +35,111 @@ public class Robot2 {
 		robot.colorSensorSide.setFloodlight(Color.WHITE);
 		LCD.drawString("running, press enter...", 0, 0);
 		Button.ENTER.waitForPress();
-		/* 1. fahre bis zum ersten Block
-		 * 2. lies Farbe des ersten Blocks
-		 * 3. fahre zum zweiten Block
-		 * 4. lies Farbe des zweiten Blocks ...
-		 */
 		LCD.clear();
-		robot.drive(5000); //distance has to be tested
-		robot.orientations[0] = robot.getColorName(robot.colorSensorSide.getColorID());
-		LCD.drawString("East " + robot.orientations[0], 0, 0); 
-		robot.drive(3000);
-		robot.orientations[1] = robot.getColorName(robot.colorSensorSide.getColorID());
-		LCD.drawString("South " + robot.orientations[1], 0, 1);
-		robot.drive(3000);
-		robot.orientations[2] = robot.getColorName(robot.colorSensorSide.getColorID());
-		LCD.drawString("West " + robot.orientations[2], 0, 2);
-		robot.drive(3000);
-		robot.orientations[3] = robot.getColorName(robot.colorSensorSide.getColorID());
-		LCD.drawString("North " + robot.orientations[3], 0, 3);
+		robot.drive(200, 0);
+		robot.measureOrientations();
+		Sound.buzz();
 		LCD.drawString("Turn Red:  " + robot.toTurn("RED"), 0, 4);
 		LCD.drawString("Turn Green:  " + robot.toTurn("GREEN"), 0, 5);
 		LCD.drawString("Turn Blue:  " + robot.toTurn("BLUE"), 0, 6);
 		LCD.drawString("Turn Yellow:  " + robot.toTurn("YELLOW"), 0, 7);
+		Delay.msDelay(2000);
+		robot.drive(-200, 500);
+		robot.turn('l', 950);
+		robot.stop();
 		Button.ENTER.waitForPress();
+		
 	}
 	
-	public void drive(int time) {
-		motorLeft.setSpeed(100);
-		motorRight.setSpeed(100);
+	public void measureOrientations () {
+		for (int i = 0; i < 4; i++) {
+			while (true) {
+				int color = colorSensorSide.getColorID();
+				if (getColorName(color) != "") {
+					Delay.msDelay(20);
+					color = colorSensorSide.getColorID();
+					if (getColorName(color) != "") {
+						break;
+					}
+				}
+				Delay.msDelay(10);
+			}
+			int[] colors = new int[4]; // red, green, blue, yellow
+			for (int j = 0; j < 5; j++) {
+				int color = colorSensorSide.getColorID();
+				switch(color) {
+				case Color.RED:
+					colors[0]++;
+					break;
+				case Color.GREEN: 
+					colors[1]++;
+					break;
+				case Color.BLUE:
+					colors[2]++;
+					break;
+				case Color.YELLOW:
+					colors[3]++;
+					break;
+				}
+				Delay.msDelay(40);
+			}
+			int index = 0;
+			for (int k = 1; k < 4; k++) {
+				int max = colors[0];
+				if (colors[k] > max) {
+					max = colors[k];
+					index = k;
+				}
+			}
+			switch(index) {
+			case 0:
+				orientations[i] = "RED";
+				break;
+			case 1:
+				orientations[i] = "GREEN";
+				break;
+			case 2:
+				orientations[i] = "BLUE";
+				break;
+			case 3:
+				orientations[i] = "YELLOW";
+				break;
+			}
+			LCD.drawString(orientations[i], 0, i); 
+			Sound.beep();
+			Delay.msDelay(250);
+		}
+	}
+	
+	public void drive(int speed, int time) {
+		motorLeft.setSpeed(speed);
+		motorRight.setSpeed(speed);
 		motorRight.forward();
 		motorLeft.forward();
 		Delay.msDelay(time);
-		stop();
 		}
-
+	
+	public void turn(char direction, int time) {
+		motorLeft.setSpeed(200);
+		motorRight.setSpeed(200);
+		if (direction == 'l') {
+			motorLeft.backward();
+			motorRight.forward();
+		}
+		else if (direction == 'r') {
+			motorLeft.forward();
+			motorRight.backward();
+		}
+		Delay.msDelay(time);
+		//motorLeft.stop(true);
+		//motorRight.stop(true);
+	}
+	
+	public void stop() {
+		motorLeft.stop(true);
+		motorRight.stop(true);
+	}
+	/*
 	public void followLine() { //currently not used
 		motorLeft.setSpeed(100);
 		motorRight.setSpeed(100);
@@ -81,12 +155,7 @@ public class Robot2 {
 		}
 
 	}
-	
-	public void stop() {
-		motorRight.stop(true);
-		motorLeft.stop(true);
-	}
-
+*/
 	public int getColorId(String color) {
 		int id = -1;
 		switch (color) {
