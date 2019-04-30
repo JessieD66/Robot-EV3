b5912bc6cea684d43a4ad9984c952ae5670ad762
JessieD66 <35700067+JessieD66@users.noreply.github.com>
2019-04-30 15:23:25 +0200

    updated measureOrientations

--- Robot2.java
+++ Robot2.java
@@ -14,19 +14,19 @@ public class Robot2 {
 	EV3MediumRegulatedMotor motorTurn;
 	EV3LargeRegulatedMotor motorLeft;
 	EV3LargeRegulatedMotor motorRight;
-	//EV3ColorSensor colorSensorLeft;
+	//EV3ColorSensor colorSensorDown;
 	//EV3ColorSensor colorSensorRight;
 	EV3ColorSensor colorSensorSide;
 	String[] orientations;
 
 	Robot2() {
-		this.motorUp = new EV3MediumRegulatedMotor(MotorPort.A);
-		this.motorTurn = new EV3MediumRegulatedMotor(MotorPort.D);
+		this.motorUp = new EV3MediumRegulatedMotor(MotorPort.D);
+		this.motorTurn = new EV3MediumRegulatedMotor(MotorPort.A);
 		this.motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
 		this.motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
-		//this.colorSensorLeft = new EV3ColorSensor(SensorPort.S1);
+		//this.colorSensorDown = new EV3ColorSensor(SensorPort.S1);
 		//this.colorSensorRight = new EV3ColorSensor(SensorPort.S2);
-		this.colorSensorSide = new EV3ColorSensor(SensorPort.S4);
+		this.colorSensorSide = new EV3ColorSensor(SensorPort.S1);
 		this.orientations = new String[] { "", "", "", "" }; // index: 0: east, 1: south, 2: west, 3: north
 	}
 
@@ -42,74 +42,76 @@ public class Robot2 {
 		LCD.drawString("Turn Green:  " + robot.toTurn("GREEN"), 0, 5);
 		LCD.drawString("Turn Blue:  " + robot.toTurn("BLUE"), 0, 6);
 		LCD.drawString("Turn Yellow:  " + robot.toTurn("YELLOW"), 0, 7);
-		Delay.msDelay(4000); //gegen Wand
-		robot.driveback(200, 1000);
-		robot.turn('l', 950);
-		robot.drive(300, 10100);
 		robot.stop();
+		robot.turn('l');
+		robot.drive(300, 10200);
+		robot.stop();
+		robot.turn('l');
+		robot.drive(200, 1600);
+		robot.stop();
+		LCD.clear();
+		LCD.drawString(robot.routerColour(robot.colorSensorSide.getColorID()), 0, 0);
+		Sound.buzz();
 		Button.ENTER.waitForPress();
 		
 	}
 	
-	public void measureOrientations () {
-		for (int i = 0; i < 4; i++) {
-			while (true) {
+	public String routerColour(int id) {
+		String name = "";
+		switch (id) {
+		case Color.BLACK:
+			name = "BLACK";
+			break;
+		case Color.WHITE:
+			name = "WHITE";
+			break;
+		default:
+			name = "NONE";
+			break;
+		}
+		return name;
+	}
+	
+	
+	public void measureOrientations() {
+		long startTime = System.currentTimeMillis();
+		while (System.currentTimeMillis() - startTime < 8100) {
+			if (System.currentTimeMillis() - startTime > 3050 && System.currentTimeMillis() - startTime < 3150) {
 				int color = colorSensorSide.getColorID();
-				if (getColorName(color) != "") {
-					Delay.msDelay(20);
-					color = colorSensorSide.getColorID();
-					if (getColorName(color) != "") {
-						break;
-					}
+				if (getColorName(color) != orientations[0] && getColorName(color) != "") {
+					orientations[0] = getColorName(color);
+					Sound.beep();
+					LCD.drawString(orientations[0], 0, 0); 
 				}
-				Delay.msDelay(10);
 			}
-			int[] colors = new int[4]; // red, green, blue, yellow
-			for (int j = 0; j < 5; j++) {
+			else if (System.currentTimeMillis() - startTime > 3900 && System.currentTimeMillis() - startTime < 4000) {
 				int color = colorSensorSide.getColorID();
-				switch(color) {
-				case Color.RED:
-					colors[0]++;
-					break;
-				case Color.GREEN: 
-					colors[1]++;
-					break;
-				case Color.BLUE:
-					colors[2]++;
-					break;
-				case Color.YELLOW:
-					colors[3]++;
-					break;
-				}
-				Delay.msDelay(40);
-			}
-			int index = 0;
-			for (int k = 1; k < 4; k++) {
-				int max = colors[0];
-				if (colors[k] > max) {
-					max = colors[k];
-					index = k;
+				if (getColorName(color) != orientations[1] && getColorName(color) != "" && getColorName(color) != orientations[0]) {
+					orientations[1] = getColorName(color);
+					Sound.beep();
+					LCD.drawString(orientations[1], 0, 1); 
 				}
 			}
-			switch(index) {
-			case 0:
-				orientations[i] = "RED";
-				break;
-			case 1:
-				orientations[i] = "GREEN";
-				break;
-			case 2:
-				orientations[i] = "BLUE";
-				break;
-			case 3:
-				orientations[i] = "YELLOW";
-				break;
+			else if (System.currentTimeMillis() - startTime > 4700 && System.currentTimeMillis() - startTime < 4800) {
+				int color = colorSensorSide.getColorID();
+				if (getColorName(color) != orientations[2] && getColorName(color) != "" && getColorName(color) != orientations[0] && getColorName(color) != orientations[1]) {
+					orientations[2] = getColorName(color);
+					Sound.beep();
+					LCD.drawString(orientations[2], 0, 2); 
+				}
 			}
-			LCD.drawString(orientations[i], 0, i); 
-			Sound.beep();
-			Delay.msDelay(250);
+			if (System.currentTimeMillis() - startTime > 5650 && System.currentTimeMillis() - startTime < 5750) {
+				int color = colorSensorSide.getColorID();
+				if (getColorName(color) != orientations[3] && getColorName(color) != "" && getColorName(color) != orientations[0] && getColorName(color) != orientations[1] && getColorName(color) != orientations[2]) {
+					orientations[3] = getColorName(color);
+					Sound.beep();
+					LCD.drawString(orientations[3], 0, 3); 
+				}
+			}
+			Delay.msDelay(5);
 		}
 	}
+
 	
 	public void drive(int speed, int time) {
 		motorLeft.setSpeed(speed);
@@ -127,18 +129,17 @@ public class Robot2 {
 		Delay.msDelay(time);
 		}
 	
-	public void turn(char direction, int time) {
+	public void turn(char direction) {
 		motorLeft.setSpeed(200);
 		motorRight.setSpeed(200);
 		if (direction == 'l') {
-			motorLeft.backward();
-			motorRight.forward();
+			motorLeft.rotate(-180, true);
+			motorRight.rotate(180);
 		}
 		else if (direction == 'r') {
-			motorLeft.forward();
-			motorRight.backward();
+			motorLeft.rotate(180, true);
+			motorRight.rotate(-180);
 		}
-		Delay.msDelay(time);
 	}
 	
 	public void stop() {
@@ -159,7 +160,6 @@ public class Robot2 {
 			motorRight.forward();
 			motorLeft.forward();
 		}
-
 	}
 */
 	public int getColorId(String color) {
@@ -221,7 +221,7 @@ public class Robot2 {
 		}
 	}
 
-	public boolean toTurn(String color) {
+	public int toTurn(String color) {
 		int colorIndex = 0;
 		int orientationIndex = 0;
 		for (int i = 0; i < 4; i++) {
@@ -245,7 +245,7 @@ public class Robot2 {
 		}
 		// first index: orientation East, South, West, North
 		// second index: color red, green, blue, yellow
-		boolean[][] turnmatrix = { { true, false, true, false }, { true, true, false, false }, { false, true, false, true }, { false, false, true, true } };
+		int[][] turnmatrix = { { 180, 90, 180, 90 }, { -90, 180, -90, 0 }, { 0, -90, 0, 90 }, { 90, 0, 90, 180 } };
 		return turnmatrix[orientationIndex][colorIndex];
 	}
 }
